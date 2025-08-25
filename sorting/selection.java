bb40c6985b43b775435dad349cce4acf515e387c
krishna <150518652+krishna-kallempeta@users.noreply.github.com>
2025-08-25 09:31:44 +0530

    sorting_techniques

---
+++ sorting/selection.java
@@ -0,0 +1,30 @@
+package sorting;
+
+
+public class selection {
+    public static void main(String args[]){
+
+       int arr [] = {4,2,5,78,45,23,9};
+
+       for(int i=0;i<arr.length-1;i++){
+           int min = i;
+           for(int j=i+1;j<arr.length;j++){
+               if(arr[j]<arr[min]){
+                   min = j;
+
+               }
+           }
+           int temp = arr[i];
+           arr[i] = arr[min];
+           arr[min] = temp;
+
+
+
+       }
+        System.out.println("after sorting");
+       for(int i=0;i<arr.length;i++){
+
+           System.out.println(arr[i]);
+       }
+    }
+}
